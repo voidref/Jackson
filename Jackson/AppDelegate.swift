bcaddf11749f4eab1812b1414b68dfe5a40d9821
Alan Westbrook <alan@rockwoodsoftware.com>
2015-06-30 23:27:07 -0700

    Initial Commit

---
+++ Jackson/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Jackson
+//
+//  Created by Alan Westbrook on 6/30/15.
+//  Copyright (c) 2015 rockwood. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
